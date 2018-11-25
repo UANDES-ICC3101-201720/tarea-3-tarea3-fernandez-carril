1af9fdc542f6da56e732a27b47ecbc4b7f8f066b
safernan12 <safernandez1@miuandes.cl>
2018-11-25 20:59:47 -0300

    Sending message server -> client

--- client_ex.py
+++ client_ex.py
@@ -11,10 +11,20 @@ s = socket.socket()         # Create a socket object
 host = socket.gethostname()  # Get local machine name
 port = 12345                # Reserve a port for your service.
 
-s.connect((host, port))
-rev_dec = s.recv(1024).decode()
-print(rev_dec)
-st = 'dis conect'
-byt = st.encode()
-s.send(byt)
-s.close  
+print("Welcome to our P2P media sharing platform!")
+while(True):
+    print("Select your option:")
+    print("1.- Find a File.")
+    print("2.- Exit.")
+    ans = input()
+    if (ans=="1"):
+        file = input("Please, select the name of the file you want to search and download.")
+        s.connect((host, port))
+        rev_dec = s.recv(1024).decode()
+        print(rev_dec)
+        fileEncode = file.encode()
+        s.send(fileEncode)
+        s.close
+
+
+
--- server_ex.py
+++ server_ex.py
@@ -12,17 +12,14 @@ s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
-
+print(host)
 s.listen(60)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
    print ('Got connection from', addr)
-   msg = 'Thank you for connecting'
+   msg = 'You are connected to the Server!'
    msg_enc = msg.encode()
-   c.sendto(msg_enc, ())
-   s.listen(1)
-   conn, addr = s.accept()
-   data = conn.recv(2000)
-   data.decode()
-   print(data)
+   c.send(msg_enc)
+   rev_dec = s.recv(1024).decode()
+   print(rev_dec)
    c.close()                # Close the connection
