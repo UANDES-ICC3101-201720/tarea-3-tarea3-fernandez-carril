c24e11084051add18b5d14011f01e9e16bf17b2d
Ignacio Carril <iacarril@miuandes.cl>
2018-11-25 23:35:06 -0300

    file try

--- client_ex.py
+++ client_ex.py
@@ -5,19 +5,20 @@ Created on Sat Nov 24 19:43:15 2018
 @author: carri
 """
 
-import socket               # Import socket module
+import socket  # Import socket module
 
-s = socket.socket()         # Create a socket object
+s = socket.socket()  # Create a socket object
 host = socket.gethostname()  # Get local machine name
-port = 12345                # Reserve a port for your service.
+port = 12345  # Reserve a port for your service.
 
 print("Welcome to our P2P media sharing platform!")
-while(True):
+while (True):
     print("Select your option:")
     print("1.- Find a File.")
     print("2.- Exit.")
+    print("3.- Tests")
     ans = input()
-    if (ans=="1"):
+    if (ans == "1"):
         file = input("Please, select the name of the file you want to search and download.")
         s.connect((host, port))
         rev_dec = s.recv(1024).decode()
@@ -25,6 +26,28 @@ while(True):
         fileEncode = file.encode()
         s.send(fileEncode)
         s.close
-
-
-
+    elif (ans == "3"):
+        s.connect((host, port))
+        filename = input("Which file do you want? (type 'q' to quit) -> ")
+        if filename != 'q':
+            s.send(filename.encode())
+            data = s.recv(1024)
+            dec_data = data.decode()
+            if dec_data[:6] == 'EXISTS':
+                filesize = len(data)
+                message = input("File Exists " + str(filesize) +
+                                " Bytes, download? (y/n) -> ")
+                if message == 'y':
+                    s.send('OK'.encode())
+                    f = open('d_' + filename, 'wb')
+                    data = s.recv(1024)
+                    totalRecv = len(data)
+                    f.write(data)
+                    while totalRecv < filesize:
+                        data = s.recv(1024).decode()
+                        totalRecv += len(data)
+                        f.write(data)
+                    print("download successful")
+        else:
+            print("File doesn't exist!")
+s.close()
--- server_ex.py
+++ server_ex.py
@@ -4,8 +4,28 @@ Spyder Editor
 
 This is a temporary script file.
 """
+import socket
+import threading
+import os
 
-import socket               # Import socket module
+def RetrFile (name, sock):
+    filename = sock.recv(1024).decode()
+    if os.path.isfile(filename):
+        sock.send(("EXISTS " + str(os.path.getsize(filename))).encode())
+        userResponse = sock.recv(1024).decode()
+        if userResponse[:2] == 'OK':
+            with open(filename, 'rb') as f:
+                bytesToSend = f.read(1024)
+                sock.send(bytesToSend)
+                while bytesToSend != "":
+                    bytesToSend = f.read(1024)
+                    sock.send(bytesToSend)
+    else:
+        sock.send("ERR".encode())
+
+    sock.close()
+
+
 
 print('Server started')
 s = socket.socket()         # Create a socket object
@@ -13,13 +33,10 @@ host = socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 print(host)
-s.listen(60)                 # Now wait for client connection.
+s.listen(5)                 # Now wait for client connection.
 while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print ('Got connection from', addr)
-   msg = 'You are connected to the Server!'
-   msg_enc = msg.encode()
-   c.send(msg_enc)
-   rev_dec = s.recv(1024).decode()
-   print(rev_dec)
-   c.close()                # Close the connection
+    c, addr = s.accept()
+    print ("client connected ip: " + str(addr))
+    file_thread = threading.Thread(target=RetrFile, args=("retrThread", c))
+    file_thread.start()
+s.close()
