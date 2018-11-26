9c18442aaa9e3e96f4d5eab376da4398aed27b90
Ignacio Carril <iacarril@miuandes.cl>
2018-11-26 04:11:19 -0300

    client partial

--- client_ex.py
+++ client_ex.py
@@ -1,18 +1,124 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 24 19:43:15 2018
-
-@author: carri
-"""
-
 import socket  # Import socket module
 import sys
+import os
+import threading
+import queue
+
+
+def send_message(connection, message):
+    try:
+        connection.sendall(message)
+    except socket.error:
+        sys.exit(-1)
+
+
+def peer_function(connection, address):
+    """
+    connect to a peer
+
+    connection : connection socket
+    address : (IP_address, port)
+    """
+    global sharing_directory
+
+    incoming_buffer = ""
+
+    while True:
+        # parse message
+        while "\0" not in incoming_buffer:
+            incoming_buffer += connection.recv(4096)
+
+        index = incoming_buffer.index("\0")
+        message = incoming_buffer[0:index-1]
+        incoming_buffer = incoming_buffer[index+1:]
+
+        fields = message.split()
+        command = fields[0]
+        # handle and respond to the message
+        if command == "GIVE":
+            file_ = sharing_directory + "/" + fields[1]
+
+            if os.path.isfile(file_):
+                # get the file size
+                file_size = os.path.getsize(file_)
+                send_message(connection, "TAKE {}\n\0".format(str(file_size)))
+                file__ = open(file_, "rb")
+                file_buffer = ""
+                file_buffer = file__.read(1024)
+                while file_buffer:
+                    print("sending: " + file_buffer)
+                    connection.send(file_buffer)
+                    file_buffer = file__.read(1024)
+                file__.close()
+            else:
+                send_message(connection, "ERROR\n\0")
+                connection.close()
+                break
+
+        elif command == "THANKS":
+            connection.close()
+            break
+
+        else:
+            send_message(connection, "ERROR\n\0")
+            connection.close()
+            break
+
+    return
+
+
+def listen(listening_ip, listening_port, queue):
+    """
+    create a server socket and start listening for incoming connections
+    """
+    try:
+        listening_socket = socket.socket()
+    except socket.error:
+        sys.exit(-1)
+
+    try:
+        listening_socket.bind((listening_ip, listening_port))
+    except socket.error:
+        sys.exit(-1)
+    # listen for incoming connections
+    listening_socket.listen(5)
+    # cli_output
+    listening_port = listening_socket.getsockname()[1]
+    # pass the listening_ip and listening_port to the main thread
+    queue.put((listening_ip, listening_port))
+    # handle incoming peer connections
+    peer_counter = 0
+    while True:
+        connection, address = listening_socket.accept()
+
+        peer_thread = threading.Thread(name="peer {}".format(peer_counter),
+                target=peer_function, args=(connection, address))
+        peer_thread.daemon = True
+        peer_thread.start()
+        peer_counter += 1
+
 
 s = socket.socket()  # Create a socket object
 host = socket.gethostname()  # Get local machine name
 port = 12345  # Reserve a port for your service.
-
+listening_ip = socket.gethostbyname()
+listening_port = 0
+sharing_directory = os.path.dirname(os.path.abspath(__file__)) + "\\sharing"
+files_list = [file_ for file_ in os.listdir(sharing_directory) if os.path.isfile(os.path.join(sharing_directory, file_))]
 print("Welcome to our P2P media sharing platform!")
+inc_buffer = ""
+s.connect(host, port)
+s.send("NEW")
+s.recv()
+queue = queue.Queue()
+listening_thread = threading.Thread(name="ListeningThread", target=listen,
+            args=(listening_ip, listening_port, queue))
+listening_thread.daemon = True
+listening_thread.start()
+listening_ip, listening_port = queue.get()
+listening_message = "LISTENING {} {}\n\0".format(listening_ip, listening_port)
+send_message(s, listening_message)
+converse(server, incoming_buffer, "LISTENING")
 while (True):
     print("Select your option:")
     print("1.- Find a File.")
